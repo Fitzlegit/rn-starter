f7910eb6263c7aa0f61aa0d41342f06ce89e78de
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-29 08:37:03 -0400

    initial commit

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SquareScreen = () => {
+  return (
+    <Text>Hello</Text>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
