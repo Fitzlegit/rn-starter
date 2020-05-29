e547bf944e1baf0ed2a12d3a2b9ab58a6427f498
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-29 13:10:21 -0400

    refactor to use a react hook reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const CounterScreen = () => {
-  const [counter, setCounter] = useState(0)
+const INCREMENT = 1
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'increase':
+      return {...state, counter: state.counter + action.payload };
+    case 'decrease':
+      return {...state, counter: state.counter - action.payload };
+    default:
+      return;
+  }
+}
+
+
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, {counter: 0});
+  const { counter } = state;
   return (
     <View>
       <Button title="Increase" onPress={() => {
-        setCounter(counter + 1);
+        dispatch({ type: 'increase', payload: INCREMENT });
       }}/>
       <Button title="Decrease" onPress={() => {
-        setCounter(counter - 1);
+        dispatch({ type: 'decrease', payload: INCREMENT });
       }} />
-      <Text>Current Count: {counter} </Text>
+    <Text>Current Count: {counter} </Text>
     </View>
   );
 };
