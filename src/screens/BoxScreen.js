2b38da0f295f1ce11ec9dea685b174a16df40dd1
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-30 08:28:52 -0400

    built out box demo using box modeland CSS flex, position props

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,13 +3,49 @@ import { View, StyleSheet, Text } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <View style={styles.viewBoxOneStyle}>
+      </View>
+      <View style={styles.viewBoxTwoStyle}>
+      </View>
+      <View style={styles.viewBoxThreeStyle}>
+      </View>
     </View>
+
   );
 };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  viewBoxOneStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    height: 50,
+    width: 50,
+
+  },
+  viewBoxTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'green',
+    height: 50,
+    width: 50,
+    top: 60,
+
+  },
+  viewBoxThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'blue',
+    height: 50,
+    width: 50,
+
+  }
+});
 
 export default BoxScreen;
